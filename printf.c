99ab0b53c3548c3de9e047b94a60a9b3fdd5652c
Catarina Macaroco <cmacaroc@f0r1s8.codam.nl>
2025-10-31 12:37:02 +0100

    printf

--- printf.c
+++ printf.c
@@ -6,7 +6,7 @@
 /*   By: cmacaroc <cmacaroc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 10:15:46 by cmacaroc          #+#    #+#             */
-/*   Updated: 2025/10/30 16:42:06 by cmacaroc         ###   ########.fr       */
+/*   Updated: 2025/10/31 12:36:12 by cmacaroc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,12 @@ int	ft_printf(const char *format, ...)
 	return (count);
 }
 
-//If you want to check what value the function returns, you can do a printf of your printf
+//If you want to check what value the function returns, 
+// you can do a printf of your printf
 // int main(void)
 // {
-//     int result = ft_printf("Sentence to know how many %s\n", "characters were written");
+//     int result = ft_printf("Sentence to know how many %s\n", 
+//     "characters were written");
 //     ft_printf("%d characters were written\n", result);
 //     ft_printf("%p\n", NULL);
 // }
